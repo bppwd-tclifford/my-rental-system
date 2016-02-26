249c27a39d1d5a6496bc4ad04be5e57a8e22c1e0
Thomas Clifford <tclifford@bppwd.com>
2016-02-26 13:45:42 +0000

    fixed administrative and public layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  layout :choose_layout
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def choose_layout
+    if current_user
+      "administrative"
+    else
+      "application"
+    end
+  end
 end
